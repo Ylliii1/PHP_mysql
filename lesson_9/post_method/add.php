c39df4566454d4a61724348e17be3ab247121f37
Ylliii1 <yllbytyqi180@gmail.com>
2024-11-27 17:00:39 +0100

    Lesson 10
    Lesson 10

--- lesson_9/post_method/add.php
+++ lesson_9/post_method/add.php
@@ -1,23 +1,33 @@
 <?php
+
+
 include_once("db.php");
 
-//isset
 
+//isset 
 if(isset($_POST['submit'])){
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+
     $sql = 'INSERT INTO users(username, email, password) VALUES (:username, :email, :password)';
 
-    $sqlQuery = $connect->prepare($sql);
 
+    $sqlQuery = $connect->prepare($sql);
     $sqlQuery->bindParam(':username', $username);
     $sqlQuery->bindParam(':email', $email);
     $sqlQuery->bindParam(':password', $password);
 
-    $sqlQuery->execute();
 
-    echo "The user was added succesfully!!!";
+    try {
+        $sqlQuery->execute();
+        echo "The user was added successfully!!!!";
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 }
+
+
+
 ?>
